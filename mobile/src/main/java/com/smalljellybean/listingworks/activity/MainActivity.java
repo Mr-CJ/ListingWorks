1a81c90966d2def31326806e99daccbe70d4182b
Jian <jianchen@thoughtworks.com>
2015-11-23 13:20:33 +0800

    add navigation menu to MainActivity

--- mobile/src/main/java/com/smalljellybean/listingworks/activity/MainActivity.java
+++ mobile/src/main/java/com/smalljellybean/listingworks/activity/MainActivity.java
@@ -1,32 +1,41 @@
 package com.smalljellybean.listingworks.activity;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.smalljellybean.listingworks.R;
 import com.smalljellybean.listingworks.activity.service.HttpService;
 import com.smalljellybean.listingworks.activity.service.ListItem;
 import com.smalljellybean.listingworks.activity.service.ListItemResponse;
+import com.smalljellybean.listingworks.base.NavigationActivity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import retrofit.RetrofitError;
 import retrofit.client.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends NavigationActivity {
 
     private static final String TAG = MainActivity.class.getName();
+    protected ListView menuList;
+    private List data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new HttpService().build().listItems(new retrofit.Callback<ListItemResponse>(){
+        PrepareData();
+        initNavigation();
+        new HttpService().build().listItems(new retrofit.Callback<ListItemResponse>() {
             @Override
             public void success(ListItemResponse listItems, Response response) {
                 Log.d(TAG, "success");
-                for(ListItem listItem : listItems.getResults()) {
+                for (ListItem listItem : listItems.getResults()) {
                     Log.d(TAG, listItem.toString());
                 }
             }
@@ -38,25 +47,34 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void initNavigation(){
+        menuList = ((ListView) navigationView.inflate());
+        SimpleAdapter adapter = new SimpleAdapter(this, data,android.R.layout.simple_list_item_1, new String[] { "AAA" },
+                new int[] { android.R.id.text1 });
+        menuList.setAdapter(adapter);
+        menuList.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+    }
+
+    private void PrepareData() {
+        data = new ArrayList<Map<String, Object>>();
+        Map<String, Object> item;
+        item = new HashMap<String, Object>();
+        item.put("1", "A");
+        item.put("2", "B");
+        data.add(item);
+        item = new HashMap<String, Object>();
+        item.put("3", "C");
+        item.put("4", "D");
+        data.add(item);
+        item = new HashMap<String, Object>();
+        item.put("5", "E");
+        item.put("6", "F");
+        data.add(item);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected int getDrawerLayoutResId() {
+        return R.layout.drawer_listview;
     }
+
 }
